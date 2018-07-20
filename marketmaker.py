7aba344542d19c8b1fa334498e025f5e2484804a
cornwarecjp <cornwarecjp@ultimatestunts.nl>
2018-07-20 19:39:05 +0200

    Add copyright notice

--- marketmaker.py
+++ marketmaker.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+#    marketmaker.py
+#    Copyright (C) 2018 by CJP
+#
+#    This file is part of Market Maker.
+#
+#    Market Maker is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    Market Maker is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with Market Maker.  If not, see <https://www.gnu.org/licenses/>.
 
 import configparser
 import json
