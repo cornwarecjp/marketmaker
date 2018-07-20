adf3cbad2033681c17209089d5113dbfe19c1503
cornwarecjp <cornwarecjp@ultimatestunts.nl>
2018-07-20 12:20:10 +0200

    Create MarketMaker class

--- marketmaker.py
+++ marketmaker.py
@@ -11,7 +11,19 @@ import bl3p
 def d(j):
 	print(json.dumps(j, sort_keys=True, indent=4, separators=(',', ': ')))
 
-# example:
+
+
+class MarketMaker:
+	def __init__(self, exchange, config):
+		self.exchange = exchange
+		d(exchange.walletHistory('BTC', 10))
+
+
+	def run(self):
+		pass
+
+
+
 config = configparser.RawConfigParser()
 
 if len(sys.argv) == 2:
@@ -22,7 +34,8 @@ else:
 public_key = config.get('bl3p', 'public_key') # ........-....-....-....-............
 secret_key = config.get('bl3p', 'secret_key') # (long string with a-z/A-Z/0-9 and =)
 
-b = bl3p.Bl3pApi('https://api.bl3p.eu/1/', public_key, secret_key)
+exchange = bl3p.Bl3pApi('https://api.bl3p.eu/1/', public_key, secret_key)
 
-d(b.walletHistory('BTC', 10))
+marketmaker = MarketMaker(exchange, config)
+marketmaker.run()
 
