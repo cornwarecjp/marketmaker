cf89c3614f01e1c6d8dcc7b7ebcbdb9de5c5e20b
cornwarecjp <cornwarecjp@ultimatestunts.nl>
2018-07-20 15:55:11 +0200

    Create the bid/ask orders

--- marketmaker.py
+++ marketmaker.py
@@ -15,10 +15,18 @@ def d(j):
 
 
 
+class Order:
+	def __init__(self, amount_funds, price):
+		self.amount_funds = amount_funds
+		self.price = price
+
+
+
 class MarketMaker:
 	def __init__(self, exchange, config):
 		self.exchange = exchange
 		self.interval    = float(config.get('marketmaker', 'interval'))
+		self.numOrders   = int(config.get('marketmaker', 'numOrders'))
 
 		minSpread   = float(config.get('marketmaker', 'minSpread'))
 		fraction    = float(config.get('marketmaker', 'fractionBTC'))
@@ -30,7 +38,13 @@ class MarketMaker:
 		self.multiplier = math.sqrt(1 + minSpread)
 		print('Multiplier: ', self.multiplier)
 
-		#TODO: cancel all active orders
+		#Cancel all active orders
+		result = self.exchange.getAllActiveOrders(self.market)
+		assert result['result'] == 'success'
+		order_ids = [order['order_id'] for order in result['data']['orders']]
+		for id in order_ids:
+			result = self.exchange.cancelOrder(self.market, id)
+			assert result['result'] == 'success'
 
 		self.balances = 0, 0
 		self.updateBalances()
@@ -57,12 +71,12 @@ class MarketMaker:
 				time.sleep(self.interval)
 			print('New balances: ', self.balances)
 
-		print('Implied price of current balances: %f %s/%s' % \
-			(
-			self.getImpliedPrice() * self.exchange.getBtcMultiplier() / self.exchange.getEurMutiplier(),
-			self.assets[0], self.assets[1]))
+		print('Implied price of current balances: ',
+			self.printablePrice(self.getImpliedPrice()))
 
 		#Enter the market
+		self.bidOrders = []
+		self.askOrders = []
 		self.placeBidOrders()
 		self.placeAskOrders()
 
@@ -75,10 +89,10 @@ class MarketMaker:
 
 	def updateBalances(self):
 		'Return value: indicates whether balances have changed'
-		balances = exchange.getBalances()
+		result = exchange.getBalances()
 		newBalances = \
 		[
-		int(balances['data']['wallets'][c]['balance']['value_int'])
+		int(result['data']['wallets'][c]['balance']['value_int'])
 		for c in self.assets
 		]
 		ret = newBalances != self.balances
@@ -87,11 +101,76 @@ class MarketMaker:
 
 
 	def placeBidOrders(self):
-		currentPrice = self.getImpliedPrice()
+		print('Placing BID orders')
+		price = self.getImpliedPrice()
+		oldBalances = self.balances
+		multiplier = self.multiplier
+		print('Implied price: ', self.printablePrice(price))
+
+		for i in range(self.numOrders):
+			price /= multiplier
+			multiplier = multiplier ** 2 #Double the step size the next time
+
+			#Prevent underflow
+			if price < 1e-100:
+				break
+
+			totalValue = oldBalances[0] + price * oldBalances[1]
+			newBalances = self.fractions[0] * totalValue, self.fractions[1] * totalValue / price
+
+			amount_funds = int(oldBalances[0] - newBalances[0])
+			print('Order: price %s, funds amount %d' % (self.printablePrice(price), amount_funds))
+			self.placeBidOrder(Order(amount_funds=amount_funds, price=price))
+
+			oldBalances = newBalances
 
 
 	def placeAskOrders(self):
-		currentPrice = self.getImpliedPrice()
+		print('Placing ASK orders')
+		price = self.getImpliedPrice()
+		oldBalances = self.balances
+		multiplier = self.multiplier
+		print('Implied price: ', self.printablePrice(price))
+
+		for i in range(self.numOrders):
+			price *= multiplier
+			multiplier = multiplier ** 2 #Double the step size the next time
+
+			#Prevent overflow
+			if price > 1e100:
+				break
+
+			totalValue = oldBalances[0] + price * oldBalances[1]
+			newBalances = self.fractions[0] * totalValue, self.fractions[1] * totalValue / price
+
+			amount_funds = int(newBalances[0] - oldBalances[0])
+			print('Order: price %s, funds amount %d' % (self.printablePrice(price), amount_funds))
+			self.placeAskOrder(Order(amount_funds=amount_funds, price=price))
+
+			oldBalances = newBalances
+
+
+	def placeBidOrder(self, order):
+		self.placeOrder(order, 'bid', self.bidOrders)
+
+
+	def placeAskOrder(self, order):
+		self.placeOrder(order, 'ask', self.askOrders)
+
+
+	def placeOrder(self, order, typeName, orderList):
+		while True:
+			result = self.exchange.addOrder(self.market, typeName,
+				order_amount_funds=order.amount_funds,
+				order_price=int(order.price * self.exchange.getBtcMultiplier()))
+			d(result)
+			if result['result'] == 'success':
+				break
+			print('%s order placement failed; retrying' % typeName)
+			time.sleep(10 * self.interval)
+
+		order.id = result['data']['order_id']
+		orderList.append(order)
 
 
 	def getImpliedPrice(self):
@@ -102,6 +181,12 @@ class MarketMaker:
 		#price * fraction[0] / fraction[1] = amount[0] / amount[1]
 		#price = (fraction[1]*amount[0]) / (fraction[0]*amount[1])
 		return (self.fractions[1]*self.balances[0]) / (self.fractions[0]*self.balances[1])
+
+
+	def printablePrice(self, price):
+		return '%s %s/%s' % (
+			price * self.exchange.getBtcMultiplier() / self.exchange.getEurMutiplier(),
+			self.assets[0], self.assets[1])
 
 
 config = configparser.RawConfigParser()
